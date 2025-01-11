e87dc8355e146879187607ceb0ea1708cce9cecf
Omar Salloum <omar.salloum@holidu.com>
2025-01-11 22:34:38 +0100

    publish to gh packages

--- AutoBuilder.Annotations/build.gradle.kts
+++ AutoBuilder.Annotations/build.gradle.kts
@@ -6,8 +6,7 @@ plugins {
 
 val GROUP_ID: String = project.properties["group.id"].toString()
 val VERSION: String = project.properties["version"].toString()
-val ARTIFACT_ID = "AutoBuilder.Annotations"
-val PUBLICATION_NAME = "autoBuilderAnnotations"
+val ARTIFACT_ID = "autobuilder.annotation"
 
 group = GROUP_ID
 version = VERSION
@@ -23,75 +22,30 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(19)
 }
 
-
-
 publishing {
-    publications {
-        repositories {
-            mavenLocal()
+    repositories {
+        mavenLocal()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/osalloum/autobuilder")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
         }
+    }
 
-        create<MavenPublication>(PUBLICATION_NAME) {
+    publications {
+        create<MavenPublication>("mavenJava") {
             from(components["java"])
             groupId = GROUP_ID
             artifactId = ARTIFACT_ID
             version = VERSION
-            pom {
-                name = "AutoBuilder.Annotations"
-                description = "AutoBuilder: A Kotlin Symbol Processing (KSP) library for automatic builder class generation. Supports default values for both primitive and non-primitive properties."
-                url = "https://github.com/osalloum/autobuilder"
-                properties = mapOf(
-                    "myProp" to "value"
-                )
-                packaging = "aar"
-                inceptionYear = "2024"
-                licenses {
-                    license {
-                        name = "The Apache License, Version 2.0"
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "osalloum"
-                        name = "Omar Salloum"
-                        email = "eyelet.taros28@icloud.com"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git@github.com:osalloum/autobuilder.git"
-                    developerConnection = "scm:git:git@github.com:osalloum/autobuilder.git"
-                    url = "https://github.com/osalloum/autobuilder"
-                }
-            }
-        }
-
-
-        signing {
-            val signingKey = providers
-                .environmentVariable("GPG_SIGNING_KEY")
-                .forUseAtConfigurationTime()
-            val signingPassphrase = providers
-                .environmentVariable("GPG_SIGNING_PASSPHRASE")
-                .forUseAtConfigurationTime()
-            if (signingKey.isPresent && signingPassphrase.isPresent) {
-                useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
-                sign(publishing.publications[PUBLICATION_NAME])
-            }
         }
     }
-}
-
-
-tasks.register<Zip>("generateZip") {
-    val publishTask = tasks.named(
-        "publish${PUBLICATION_NAME.replaceFirstChar { it.uppercaseChar() }}PublicationToMavenLocalRepository",
-        PublishToMavenRepository::class.java
-    )
-    from(publishTask.map { it.repository.url })
-    archiveFileName.set("autobuilder-annotations_${VERSION}.zip")
 }
--- AutoBuilder.Processor/build.gradle.kts
+++ AutoBuilder.Processor/build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     kotlin("jvm")
     id("maven-publish")
-    signing
 }
 
 val GROUP_ID: String = project.properties["group.id"].toString()
 val VERSION: String = project.properties["version"].toString()
-val ARTIFACT_ID = "AutoBuilder.Processor"
-val PUBLICATION_NAME = "autoBuilderProcessor"
+val ARTIFACT_ID = "autobuilder.processor"
 
 group = GROUP_ID
 version = VERSION
@@ -39,68 +37,26 @@ kotlin {
     jvmToolchain(19)
 }
 
-publishing {
-    publications {
-        repositories {
-            mavenLocal()
-        }
 
-        create<MavenPublication>(PUBLICATION_NAME) {
+publishing {
+    repositories {
+        mavenLocal()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/osalloum/autobuilder")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
             from(components["java"])
             groupId = GROUP_ID
             artifactId = ARTIFACT_ID
             version = VERSION
-            pom {
-                name = "AutoBuilder.Processor"
-                description = "AutoBuilder: A Kotlin Symbol Processing (KSP) library for automatic builder class generation. Supports default values for both primitive and non-primitive properties."
-                url = "https://github.com/osalloum/autobuilder"
-                properties = mapOf(
-                    "myProp" to "value"
-                )
-                packaging = "aar"
-                inceptionYear = "2024"
-                licenses {
-                    license {
-                        name = "The Apache License, Version 2.0"
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "osalloum"
-                        name = "Omar Salloum"
-                        email = "eyelet.taros28@icloud.com"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git@github.com:osalloum/autobuilder.git"
-                    developerConnection = "scm:git:git@github.com:osalloum/autobuilder.git"
-                    url = "https://github.com/osalloum/autobuilder"
-                }
-            }
-        }
-
-
-        signing {
-            val signingKey = providers
-                .environmentVariable("GPG_SIGNING_KEY")
-                .forUseAtConfigurationTime()
-            val signingPassphrase = providers
-                .environmentVariable("GPG_SIGNING_PASSPHRASE")
-                .forUseAtConfigurationTime()
-            if (signingKey.isPresent && signingPassphrase.isPresent) {
-                useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
-                sign(publishing.publications[PUBLICATION_NAME])
-            }
         }
     }
-}
-
-tasks.register<Zip>("generateZip") {
-    val publishTask = tasks.named(
-        "publish${PUBLICATION_NAME.replaceFirstChar { it.uppercaseChar() }}PublicationToMavenLocalRepository",
-        PublishToMavenRepository::class.java
-    )
-    from(publishTask.map { it.repository.url })
-    archiveFileName.set("autobuilder-processor_${VERSION}.zip")
 }
